62f2a71b1ed996d2f22359b18ff079962a42a950
Oshadini Kariyawasam <43576871+Oshadini@users.noreply.github.com>
2024-05-06 00:07:56 +0530

    Update MuRAGAppv3.py

--- MuRAGAppv3.py
+++ MuRAGAppv3.py
@@ -415,7 +415,7 @@ query = st.text_input("Ask a Question from the PDF Files", key="user_question")
 pr2 = st.button("Generate")
 if pr2:
     retriever_multi_vector_imgstt = st.session_state.retriever_multi_vector_img
-    if retriever_multi_vector_img is not None:
+    if retriever_multi_vector_imgstt is not None:
       chain_multimodal_rag = multi_modal_rag_chain(retriever_multi_vector_imgstt)
       docs = retriever_multi_vector_img.get_relevant_documents(query, limit=1)
       markdown_text = chain_multimodal_rag.invoke(query)
