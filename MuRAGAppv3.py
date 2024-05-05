4fb6905e37d6905e3c4e8aae1801ed276b577571
Oshadini Kariyawasam <43576871+Oshadini@users.noreply.github.com>
2024-05-05 21:42:21 +0530

    Update MuRAGAppv3.py

--- MuRAGAppv3.py
+++ MuRAGAppv3.py
@@ -67,6 +67,7 @@ openai.api_key = st.secrets["OPENAI_API_KEY"]
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
 uploaded_file = st.file_uploader(label = "Upload your file",type="pdf")
+user_question = st.text_input("Ask a Question from the PDF Files", key="user_question")
 pr = st.button("Process")
 if pr ==True:
     temp_file="./temp.pdf"
@@ -86,7 +87,7 @@ if pr ==True:
         image_output_dir_path=image_path
     )
 st.write(pdf_elements)
-user_question = st.text_input("Ask a Question from the PDF Files", key="user_question")
+
 # Categorize elements by type
 def categorize_elements(raw_pdf_elements):
   """
