8e4974f77b40315db4b1458a4970c754677ac999
Oshadini Kariyawasam <43576871+Oshadini@users.noreply.github.com>
2024-05-05 21:27:29 +0530

    Update MuRAGAppv3.py

--- MuRAGAppv3.py
+++ MuRAGAppv3.py
@@ -87,20 +87,20 @@ if uploaded_file is not None:
             image_output_dir_path=image_path
         )
 
-        # Categorize elements by type
-        def categorize_elements(_raw_pdf_elements):
-          """
-          Categorize extracted elements from a PDF into tables and texts.
-          raw_pdf_elements: List of unstructured.documents.elements
-          """
-          tables = []
-          texts = []
-          for element in _raw_pdf_elements:
-              if "unstructured.documents.elements.Table" in str(type(element)):
-                  tables.append(str(element))
-              elif "unstructured.documents.elements.CompositeElement" in str(type(element)):
-                  texts.append(str(element))
-          return texts, tables
+# Categorize elements by type
+def categorize_elements(_raw_pdf_elements):
+  """
+  Categorize extracted elements from a PDF into tables and texts.
+  raw_pdf_elements: List of unstructured.documents.elements
+  """
+  tables = []
+  texts = []
+  for element in _raw_pdf_elements:
+      if "unstructured.documents.elements.Table" in str(type(element)):
+          tables.append(str(element))
+      elif "unstructured.documents.elements.CompositeElement" in str(type(element)):
+          texts.append(str(element))
+  return texts, tables
         
         
 
